969181c66e6c4a16f0633c1d783c382d37e530ca
bagdenius <bagdensparrow@gmail.com>
2025-07-14 19:01:21 +0300

    feat: add core components and styles for navigation, user interface, and pages
    - Created AppNav component for application navigation.
    - Added PageNav component for page navigation with links.
    - Implemented City, CountryItem, and User components to display relevant information.
    - Developed Form component for user input with validation.
    - Designed various CSS modules for styling components.
    - Established Homepage, Login, Product, and Pricing pages with layout and content.
    - Integrated global styles and variables for consistent theming.
    - Set up Vite configuration for project build and development.

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
